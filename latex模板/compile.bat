c6b8625660fac924dbb5a343eb14a1ce29e7b2ca
peng-1998 <1471881859@qq.com>
2022-04-21 07:46:12 +0000

    1

--- latex模板/compile.bat
+++
@@ -1,8 +0,0 @@
-xelatex unet.tex
-bibtex unet   
-xelatex unet.tex
-bibtex unet   
-xelatex unet.tex
-del *.aux *.log *.nav *.out *.snm *.bak  *.sav *.gz *.toc
-del *.bak *.aux *.log *.sav *.bbl *.blg *.gz*
-unet.pdf
